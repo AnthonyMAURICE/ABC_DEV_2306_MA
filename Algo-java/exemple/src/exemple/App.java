3822dd7cc858359402669eb231f1bc77978de38d
AnthonyMAURICE <mauriceanthony.pro@gmail.com>
2023-06-27 16:57:09 +0200

    java exercices

--- Algo-java/exemple/src/exemple/App.java
+++ Algo-java/exemple/src/exemple/App.java
@@ -1,10 +1,38 @@
 package exemple;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		//String welcome = "bienvenue sur Eclipse !";
+		//System.out.println("Hello World, " + welcome);
+		/*
+		VARIABLE
+	    N1 est un entier
+	    N2 est un entier
+	    Resultat est un réel
+		Début programme
+		    Ecrire "Saisir le premier nombre : "
+		    Lire <-- N1
+		    Ecrire "Saisir le second nombre : "
+		    Lire <-- N2
+		    Resultat <-- (N1+N2)/2
+		    Ecrire "La moyenne des deux nombres saisis est : ",Resultat
+		Fin programme
+		 */
+		float n1;
+		float n2;
+		float result;
+		Scanner scanner = new Scanner(System.in);
+			System.out.println("Saisissez le premier nombre : ");
+			n1 = scanner.nextFloat();
+			System.out.println("Saisissez le second nombre : ");
+			n2 = scanner.nextFloat();
+			result = (n1 + n2)/2;
+			System.out.println("La moyenne des deux nombres est : " + result);
+		scanner.close();
 	}
 
 }
