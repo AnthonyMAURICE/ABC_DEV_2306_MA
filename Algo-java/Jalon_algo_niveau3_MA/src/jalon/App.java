58b68644c443be25ab0f5b6de6c82dffe48fbb0a
AnthonyMAURICE <mauriceanthony.pro@gmail.com>
2023-07-20 14:52:12 +0200

    jalon 3, modifications et commentaires

--- Algo-java/Jalon_algo_niveau3_MA/src/jalon/App.java
+++ Algo-java/Jalon_algo_niveau3_MA/src/jalon/App.java
@@ -4,24 +4,24 @@ public class App {
 
 	public static void main(String[] args) {
 		int nombre, multiplicateur, resultat;
-		String reload = "o";
+		String reload = "o"; // initialisée à "o" pour qu'il y ait un nouveau passage dans la boucle en cas de saisie hors paramètes
 		
 		Scanner scanner = new Scanner(System.in);
 			
-			do {
+			while(reload.equals("o")){ // boucle tant que reload est égal à "o"
 				System.out.println("Saisir un nombre entre 1 et 10");
 				nombre = scanner.nextInt();
-				if(nombre < 10 && nombre > 1) {
-					for(multiplicateur = 0; multiplicateur <= 10; multiplicateur++) {
+				if(nombre < 10 && nombre > 1) { // contrôle si la saisie est dans les paramètres demandés
+					for(multiplicateur = 0; multiplicateur <= 10; multiplicateur++) { // boucle for qui fait le calcul et l'affichage des tables
 						resultat = nombre * multiplicateur;
 						System.out.println(multiplicateur + " * " + nombre + " = " + resultat);
 					}
 					System.out.println("Souhaitez-vous afficher une autre table ? Oui(O) ou Non(N)");
-					reload = scanner.next().toLowerCase();
-				}else {
+					reload = scanner.next().toLowerCase(); // input qui détermine la sortie ou non de la boucle
+				}else { // sinon affiche un message d'erreur
 					System.out.println("Vous avez saisi un nombre hors parametres, veuillez recommencer.");
 				}
-			}while(reload.equals("o"));
+			}
 		
 			System.out.println("Fin du programme");
 		scanner.close();
