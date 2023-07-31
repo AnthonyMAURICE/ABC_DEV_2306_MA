d63156cb10c3357febc2bdf75cbd528862f23050
AnthonyMAURICE <mauriceanthony.pro@gmail.com>
2023-07-31 16:52:22 +0200

    java exercices fonction

--- Algo-java/exemple/src/fonction7/App.java
+++ Algo-java/exemple/src/fonction7/App.java
@@ -7,57 +7,64 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		boolean encore = false, trouve = false, victoire = false;
+		boolean encore = false; 
 		
 		Scanner scanner = new Scanner(System.in);
 			do {
 				int nombreChoisi, nombre, essais = 0, nombreOrdi = 0, upFork = 100, downFork = 1; // variables déclarées et initilisées ici
 				String rejouer;																	// car besoin de les réinitialiser à chaque 
-				encore = false;																	// début de partie pour la nombre de l'ordinateur
+				boolean trouve = false, victoire = false;										// début de partie
+				encore = false;																	
 				nombre = fourchette(1, 100);													// généré aléatoirement
 				
 				do {
 					System.out.println(nombre);
 					System.out.println("Entrez un nombre, tentez de trouver le bon.");
 					nombreChoisi = scanner.nextInt();
-					if(nombreChoisi == nombre) {
+					
+					if(nombreChoisi == nombre) { // passe les deux booléens à true en cas de victoire
 						victoire = true;
 						trouve = true;
-					}else {
+					}else { // sinon, indication de positionnement et au tour de l'ordinateur "d'essayer"
+						
 						if(nombreChoisi < nombre) {
 							System.out.println("Vous êtes en dessous.");
 						}else {
 							System.out.println("Vous êtes au dessus.");
 						}
-						nombreOrdi = fourchette(downFork, upFork); // condition qui permet "d'affiner" le résultat de l'ordinateur
-						if(nombreOrdi > nombre) {					// avec des fourchettes hautes et basses
-							upFork = nombreOrdi;
+						nombreOrdi = fourchette(downFork, upFork);  // arguments "d'affinage" du résultat de l'ordinateur
+						
+						if(nombreOrdi > nombre) {					// condition qui permet "d'affiner" le résultat de l'ordinateur
+							upFork = nombreOrdi;					// avec des fourchettes hautes et basses
 						}else{
 							downFork = nombreOrdi;
 						}
 						
 						System.out.println(nombreOrdi);
-						if(nombreOrdi == nombre) {
-							victoire = false;
+						
+						if(nombreOrdi == nombre) { // quand l'ordinateur trouve, le jeu s'arrête mais le booléen de victoire reste à false
 							trouve = true;
 						}
+						
 					}
 					essais++;
 				}while(!trouve);
+				
 				if(victoire) {
 					System.out.println("Bravo, vous avez gagné en " + essais + " coup(s)");
 				}else {
 					System.out.println("Désolé, SkyNet vous a battu");
 				}
-				victoire = false;
-				trouve = false;
+			
 				System.out.println("Une autre partie ? (O pour oui)");
 				rejouer = scanner.next().toLowerCase();
+				
 				if(rejouer.equals("o")) {
 					encore = true;
 				}else {
 					encore = false;
 				}
+				
 			}while(encore);
 		
 			System.out.println("Merci d'avoir joué.");
