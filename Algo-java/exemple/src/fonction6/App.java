89ef4d5fa75c252b4442b9e381a14d8f904b9be8
AnthonyMAURICE <mauriceanthony.pro@gmail.com>
2023-08-02 12:50:59 +0200

    java exercices modifications et optimisation

--- Algo-java/exemple/src/fonction6/App.java
+++ Algo-java/exemple/src/fonction6/App.java
@@ -7,15 +7,12 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int nombre1, nombre2;
-		boolean estDivisible = false;
 		Scanner scanner = new Scanner(System.in);
 			System.out.println("Entrez le premier nombre : ");
 			nombre1 = scanner.nextInt();
 			System.out.println("Entrez le deuxième nombre : ");
 			nombre2 = scanner.nextInt();
-			estDivisible = divisible(nombre1, nombre2);
-			
-			if (estDivisible == true) {
+			if (divisible(nombre1, nombre2)) { // fonction passée en paramètre de condition
 				System.out.println(nombre1 + " est divisible par " + nombre2);
 			}else {
 				System.out.println(nombre1 + " n'est pas divisible par " + nombre2);
