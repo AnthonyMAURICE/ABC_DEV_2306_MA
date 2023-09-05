19378278c6f725b075590d436ceb22fa5d9baee7
AnthonyMAURICE <mauriceanthony.pro@gmail.com>
2023-09-05 10:54:44 +0200

    java object exercice robot save9.1

--- Algo-java/Projet_robot_ABCDev/src/robot/App.java
+++ Algo-java/Projet_robot_ABCDev/src/robot/App.java
@@ -7,8 +7,8 @@ public class App {
 	public static void main(String[] args) {
 		
 		//variables de test
-		boolean test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, testMixer, testMixer2;
-		int posX, posY;
+		boolean test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, testMixer, testMixer2, testMixer3;
+		int cylonPosX, cylonPosY;
 				
 		Robot cylon = new Robot("Cylon", "WarBot", 200, true, true, false, 10, 0); //objet Robot construit avec paramètres
 		cylon.setPower();
@@ -18,8 +18,8 @@ public class App {
 		cylon.setMouvement(Mouvement.AVANT); // test si le cylon avance bien sur le bon axe suite à sa rotation à gauche
 		test3 = cylon.setMouvement(Mouvement.DROITE);
 		cylon.setMouvement(Mouvement.AVANT); // même test que précédement, cette fois après rotation à droite  
-		posX = cylon.getPosX(); // retour des positions du cylon sur les axes x et y pour vérifier qu'il se déplace sur les bons axes suite aux rotations
-		posY = cylon.getPosY();
+		cylonPosX = cylon.getPosX(); // retour des positions du cylon sur les axes x et y pour vérifier qu'il se déplace sur les bons axes suite aux rotations
+		cylonPosY = cylon.getPosY();
 		test4 = cylon.takeObject(); // renvoit false, pas de scan préalable
 		cylon.scan();
 		test5 = cylon.takeObject(); // renvoit true, scan effectué juste avant
@@ -41,6 +41,8 @@ public class App {
 		Robot mixer = new Robot("Mixer", "Robot de cuisine", 50, false, false, true, 10, 10); // création d'un robot de cuisine, non mobile
 		testMixer = mixer.setMouvement(Mouvement.AVANT); // renvoit true car la commande est valide, mais ne déplace pas le robot car non mobile
 		testMixer2 = mixer.setMouvement(Mouvement.AGIR); // renvoit true, action de base du robot, à modeler selon le type
+		mixer.scan(); // parce que pourquoi pas, un mixer avec scan d'ingrédients par exemple
+		testMixer3 = mixer.takeObject(); // renverra false, un mixer ne peut saisir un objet
 		
 	}
 
