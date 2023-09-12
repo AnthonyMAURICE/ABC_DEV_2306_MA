3be35630c13294fe325dc14ec1bb6cfee0a9dd2d
AnthonyMAURICE <mauriceanthony.pro@gmail.com>
2023-09-12 12:21:12 +0200

    java jalon objet 12_09_2023 petite correction2

--- Algo-java/Jalon_Objet_MA/src/jalon/App.java
+++ Algo-java/Jalon_Objet_MA/src/jalon/App.java
@@ -5,8 +5,13 @@ public class App {
 	public static void main(String[] args) {
 		Article article1 = new Article(42, "Produit 1", 214.5);
 		Article article2 = new Article(314, "Produit 2", 108);
+		Article articleTemoin = new Article();
 		
-		System.out.println("Prix TTC de l'article : " + article1.calculerPrixTTC() + ", son prix hors taxes était de : " + article1.getPrixHT());
+		System.out.println("Prix TTC de l'article 1 : " + article1.calculerPrixTTC() + ", son prix hors taxes était de : " + article1.getPrixHT());
+		System.out.println("Prix TTC de l'article 2 : " + article2.calculerPrixTTC() + ", son prix hors taxes était de : " + article2.getPrixHT());
+		System.out.println("Prix TTC de l'article témoin (constructeur par défaut) : " + articleTemoin.calculerPrixTTC() + ", son prix hors taxes était de : " + articleTemoin.getPrixHT());
+		
+		
 		
 		article2.afficherArticle();
 	}
